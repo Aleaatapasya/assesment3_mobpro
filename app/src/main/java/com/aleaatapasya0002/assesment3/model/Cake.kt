5e4ee51c964bb68c345e0106e0fe08294a11920d
Alea Atapasya <aleaatpsya@gmail.com>
2025-06-09 21:58:59 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/aleaatapasya0002/assesment3/model/Cake.kt
@@ -0,0 +1,7 @@
+package com.aleaatapasya0002.assesment3.model
+
+data class Cake(
+    val namaKue: String,
+    val deskripsi: String,
+    val imageId: String
+)
--- app/src/main/java/com/aleaatapasya0002/assesment3/network/CakeApiService.kt
+++ app/src/main/java/com/aleaatapasya0002/assesment3/network/CakeApiService.kt
@@ -1,17 +1,25 @@
 package com.aleaatapasya0002.assesment3.network
 
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.aleaatapasya0002.assesment3.model.Cake
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit.Builder
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://store.sthresearch.site/"
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+private val retrofit = Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
+
 interface CakeApiService {
     @GET("cake.php")
-    suspend fun getCake(): String
+    suspend fun getCake(): List<Cake>
 }
 object CakeApi{
     val service: CakeApiService by lazy{
